3c9e65d65a066038c03be42cf0d33025de7a03a0
Yrmee <80338690+Yrmee@users.noreply.github.com>
2021-05-06 17:38:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Hello Winc Academy");
+
+let myName = 'Yrja';
+console.log(myName);
+
+// Je kan geen variablen met if maken, omdat dit een reserverd keyword is. //
+
+let numbers = 4 + 4;
+console.log(numbers);
+
+let strings = "4" + "4";
+console.log(strings);
+// Dit kan, maar het wordt dan 44 ipv 8 //
+
+let addition = 1 + 1;
+console.log(addition);
+
+let subtractrion = 1 - 1;
+console.log(subtractrion);
+
+let multiplication = 8 * 8;
+console.log(multiplication);
+
+let division = 2 / 4;
+console.log(division);
+
+let modulus = 5 % 3;
+console.log(modulus);
+
+let age = "21";
+console.log(typeof age);
